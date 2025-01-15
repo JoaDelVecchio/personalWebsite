2387b0e80a4eaaf8168053ce212da65a85c5c42e
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-01-15 17:25:56 -0300

    change style

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,11 @@ function App() {
       description:
         "Momentum is a modern and minimalist task management web application designed to boost productivity. With an elegant design and seamless user experience, it allows users to organize daily tasks, track progress, and achieve their goals effectively.",
       technologies: [
-        "React/Typescript",
+        "React",
         "Node.js/Express",
         "Supabase (PostgreSQL)",
         "TailwindCSS",
+        "Typescript",
       ],
     },
     {
